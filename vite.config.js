1b0955cf92f89a8dd1ee89d87c1fc07a0a2c131f
chino brian <bchinoyanarico@frba.utn.edu.ar>
2025-05-09 12:44:24 -0300

    intentando deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/proyecto-front-react/',
   plugins: [react()],
+  base: 'https://brianchino.github.io/proyecto-front-react/',
 })
